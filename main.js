ff846dcfa89630fe8b10af88ae4a770690a3b525
manasa1324 <79976512+manasa1324@users.noreply.github.com>
2021-12-13 15:44:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function bedroom()
+{
+    window.location = "bedroom.html";
+}
+
+function living()
+{
+    window.location = "living.html";
+}
+
+function balcony()
+{
+    window.location = "balcony.html";
+}
+
+function bathroom()
+{
+    window.location = "bathroom.html";
+}
+
+function kitchen()
+{
+    window.location = "kitchen.html";
+}
